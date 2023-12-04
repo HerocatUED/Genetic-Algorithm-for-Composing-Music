4f40fdb30f1f5a651d83787514c8f8884fde861b
HerocatUED <1722791503@qq.com>
2023-12-04 16:40:03 +0800

    update

--- fitness_functions/chord.py
+++ fitness_functions/chord.py
@@ -3,3 +3,12 @@ import numpy as np
 import sys
 sys.path.append('..')
 from definitions import *
+
+
+def chord_score(music:np.array):
+    '''
+    Args:
+    music: 2D array
+    '''
+    #TODO
+    pass
