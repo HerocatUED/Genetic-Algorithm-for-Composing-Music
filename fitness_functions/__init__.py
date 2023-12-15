9583419f6eb51b7cfe8d692a533218dd1c6e16d4
bajimh <xiuluo1054689699@126.com>
2023-12-15 20:23:34 +0800

    update

--- fitness_functions/__init__.py
+++ fitness_functions/__init__.py
@@ -4,11 +4,12 @@ from melody import melody_score
 from chord import chord_score
 from rhythm import rhythm_score
 
-def fitness_function(music:np.array):
-    '''
+
+def fitness_function(music: np.array):
+    """
     Args:
     music: 2D array
-    '''
+    """
     assert len(np.shape(music)) == 2
-    
-    return melody_score(music) + 2 * chord_score(music) + rhythm_score(music)
+
+    return melody_score(music) + chord_score(music) + rhythm_score(music)
