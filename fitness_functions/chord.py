c30d73388995dedba42a02942b60c870676d92a6
bajimh <xiuluo1054689699@126.com>
2023-12-15 17:40:50 +0800

    update by hzj

--- fitness_functions/chord.py
+++ fitness_functions/chord.py
@@ -1,63 +1,75 @@
 import numpy as np
 
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 from definitions import *
 
-chord_trans = [1,1,2,2,3,4,4,5,5,6,6,0] #将12音级暂时转化为0~6，其中1为do,...,6为la,0为si
+chord_trans = [1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 6, 0]  # 将12音级暂时转化为0~6，其中1为do,...,6为la,0为si
 
-def in5(a:int,b:int): # 在五和弦里
-    if a == b or a == (b+2)%7 or a == (b+4)%7:
+
+def in5(a, b):  # 在五和弦里
+    if a == b or a == (b + 2) % 7 or a == (b + 4) % 7:
         return True
     return False
 
-def in7(a:int,b:int): # 在七和弦里
-    if(in5(a,b)):
+
+def in7(a, b):  # 在七和弦里
+    if in5(a, b):
         return True
-    if a == (b+6)%7:
+    if a == (b + 6) % 7:
         return True
     return False
 
-def calc(len:int,a:np.array,rate7:float,chord_B:float,rate7_penalty:float):
-    '''
+
+def calc(
+    length: int, a: np.ndarray, rate7: float, chord_B: float, rate7_penalty: float
+):
+    """
     chords:和弦库
     chords_type:和弦属性
-    '''
+    """
     ans = 0.0
-    (n,) = np.shape(chords)
-    for i in range(len):
-        a[i] %= 12
-    for i in range(n):
-        m = np.shape(chords)
+    a = a % 12
+    for chord, chord_type in zip(chords, chords_type):
+        # Fixed by hzj
+        m = len(chord)
         mnnum7 = 0.0
         mxnum7 = 0.0
         neq = False
-        if len != m:
+        if length != m:
             continue
         for j in range(m):
-            if in7(a[j],chords[i][j]):
+            if in7(a[j], chord[j]):
                 mxnum7 += 1
-                if in5(a[j],chords[i][j]) == False:
+                if in5(a[j], chord[j]) == False:
                     mnnum7 += 1
             else:
                 neq = True
                 break
         if neq == True:
             continue
-        mxnum7 /= len
-        mnnum7 /= len
+        mxnum7 /= length
+        mnnum7 /= length
         nwans = 1.0
-        if(chords_type[i] == 1): # A : 0 , B : 1
+        if chord_type == 1:  # A : 0 , B : 1 # Fixed definitions.py by hzj
             nwans = chord_B
-        if(mxnum7 < rate7):
-            ans = ans * (1-(rate7-mxnum7)*rate7_penalty)
-        if(mnnum7 > rate7):
-            ans = ans * (1-(mnnum7 - rate7)*rate7_penalty)
-        ans = max(ans,nwans)
+        if mxnum7 < rate7:
+            ans = ans * (1 - (rate7 - mxnum7) * rate7_penalty)
+        if mnnum7 > rate7:
+            ans = ans * (1 - (mnnum7 - rate7) * rate7_penalty)
+        ans = max(ans, nwans)
     return ans
 
-def chord_score(music:np.array,not_in_chord:float = 0.8,rate7:float = 0.25,chord_B:float = 0.9,rate7_penalty:float = 0.1):
-    '''
+
+def chord_score(
+    music: np.ndarray,
+    not_in_chord: float = 0.8,
+    rate7: float = 0.25,
+    chord_B: float = 0.9,
+    rate7_penalty: float = 0.1,
+):
+    """
     Args:
     music: 2D array
     fi : 截止到第i-1个位置的最高分
@@ -65,33 +77,38 @@ def chord_score(music:np.array,not_in_chord:float = 0.8,rate7:float = 0.25,chord
     rate7_penalty : 七和弦比例偏差导致的扣分程度
     chord_B : B和弦走向的扣分系数
     not_in_chord : 一个小节无法匹配进和弦的扣分系数
-    '''
-    (n,m) = np.shape(music)
-    mm:int = m/8
-    nw = np.zeros((n,mm))
+    """
+    (n, m) = np.shape(music)
+    mm = m // 8
+    nw = np.zeros((n, mm))
     score = np.zeros(n)
-    for i in range(n):
-        for j in range(0,m,8):
-            if music[i][j]!=0 :
-                nw[i][j/8]=chord_trans[music[i][j]%12]
-                continue
-            if j!=0:
-                if music[i][j-1]!=0:
-                    nw[i][j/8]=chord_trans[music[i][j-1]%12]
-                    continue
-            for k in range(1,8):
-                if music[i][j+k]!=0:
-                    nw[i][j/8]=chord_trans[music[i][j+k]%12]
-                    break
 
+    # Fixed by hzj 
     for i in range(n):
-        f = np.zeros(mm+1)
-        f[0]=100
+        for j in range(0, m, 8):
+            if music[i][j] != 0:
+                nw[i][j // 8] = chord_trans[music[i][j] % 12]
+                continue
+            if j != 0:
+                if music[i][j - 1] != 0:
+                    nw[i][j // 8] = chord_trans[music[i][j - 1] % 12]
+                    continue
+            for k in range(1, 8):
+                if music[i][j + k] != 0:
+                    nw[i][j // 8] = chord_trans[music[i][j + k] % 12]
+                    break
+        
+        f = np.zeros(mm + 1)
+        f[0] = 100
         for j in range(mm):
-            f[j+1]=f[j]*not_in_chord
+            f[j + 1] = f[j] * not_in_chord
+
             for k in range(j):
-                f[j+1]=max(f[j+1],f[k]*calc(j-k+1,nw[i][k:j],rate7,chord_B,rate7_penalty))
-        score[i]=f[mm]
+                f[j + 1] = max(
+                    f[j + 1],
+                    f[k]
+                    * calc(j - k + 1, nw[i][k : j + 1], rate7, chord_B, rate7_penalty),
+                )
+        score[i] = f[mm]
 
     return score
-    pass
---
+++ pipeline.py
@@ -0,0 +1,43 @@
+# 使用遗传算法生成音乐，包括和弦、旋律、节奏等评分
+import sys
+import os
+import numpy as np
+
+sys.path.append("./fitness_functions")
+from fitness_functions import fitness_function
+from fitness_functions.chord import chord_score
+from fitness_functions.melody import melody_score
+from fitness_functions.rhythm import rhythm_score
+from data.midi import read_mid
+from pathlib import Path
+
+
+def main(data_dir: Path, save_path: Path):
+    # read midis
+    print("Reading midi files...")
+    midi_paths = list(data_dir.glob("*.mid"))
+    musics = np.stack([read_mid(path) for path in midi_paths], axis=0)
+    print(musics.shape)
+    # calculate fitness
+    print("Calculating fitness...")
+    fitness = fitness_function(musics)
+    chord_losses = chord_score(musics)
+    melody_losses = melody_score(musics)
+    rhythm_losses = rhythm_score(musics)
+    for path, fit, chord_loss, melody_loss, rhythm_loss in zip(
+        midi_paths, fitness, chord_losses, melody_losses, rhythm_losses
+    ):
+        print(
+            f"{path.name:\u3000<11} [fit: {fit:7.3f}], [chord: {chord_loss:7.3f}], [melody: {melody_loss:7.3f}], [rhythm: {rhythm_loss:7.3f}]"
+        )
+    return
+    # calculate fitness
+
+
+if __name__ == "__main__":
+    # 存放midi文件的路径
+    data_dir = Path("../mididata")
+    save_path = Path("./result")
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    main(data_dir=data_dir, save_path=save_path)
