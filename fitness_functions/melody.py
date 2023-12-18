17804e8cbcfee78111f10c8c001664e7ec1f860a
HerocatUED <1722791503@qq.com>
2023-12-19 00:04:33 +0800

    update

--- fitness_functions/melody.py
+++ fitness_functions/melody.py
@@ -15,20 +15,24 @@ def repeatness(music:np.array, threshold:float):
     '''
     # calculate repeatness
     music_num = np.shape(music)[0]
-    max_rate = np.zeros(music_num)
-    for i in range(music_num):
-        _, counts = np.unique(music[i], return_index=False, return_inverse=False, return_counts=True)
-        max_rate[i] = np.max(counts)
+    max_rate = np.zeros(music_num*16)
+    music1 = music.reshape((-1, 8)).copy()
+    for i in range(music_num*16):
+        _, counts = np.unique(music1[i], return_index=False, return_inverse=False, return_counts=True)
+        max_rate[i] = np.max(counts)/8
+    max_rate[max_rate<threshold] = 0
+    max_rate = max_rate.reshape((-1, 16)).sum(axis=1)
     # score with repeatness
     score = np.ones(music_num) * 100
-    score[max_rate>threshold] -= max_rate[max_rate>threshold]
+    score[max_rate>threshold] -= max_rate * 5
     return np.clip(score, a_min=0, a_max=100)
 
 
-def fluctuation(music:np.array, threshold:float, threshold_2:float):
+def fluctuation(music:np.array, nonzero_cnt: int, threshold:float, threshold_2:float):
     '''
     Args:
     music: 2D array
+    nonzero_cnt: num of nonzero
     threshold: maximum average fluctuation
     threshold_2: maximum fluctuation
     '''
@@ -36,12 +40,12 @@ def fluctuation(music:np.array, threshold:float, threshold_2:float):
     music_1 = np.pad(music, ((0,0),(1,0)), 'constant', constant_values=0)
     music_2 = np.pad(music, ((0,0),(0,1)), 'constant', constant_values=0)
     # calculate fluctuation
-    det_music = (music_2 - music_1)[:, 1:-1]
+    det_music = np.abs((music_2 - music_1)[:, 1:-1])
     # det_music[det_music>36] = 0 # consider rest pitch
-    avg_fluctuation = np.sum(np.abs(det_music), axis=1) / music_length
+    avg_fluctuation = np.sum(det_music, axis=1) / nonzero_cnt
     det_music[det_music<=threshold_2] = 0
     det_music[det_music>0] = 1
-    max_fluctuation = np.sum(np.abs(det_music), axis=1)
+    max_fluctuation = np.sum(det_music, axis=1)
     # score with fluctuation
     score = np.ones( np.shape(music)[0]) * 100
     score[avg_fluctuation>threshold] -= avg_fluctuation[avg_fluctuation>threshold] * 5
@@ -63,10 +67,12 @@ def melody_score(
     weight_f: weight of fluction score
     '''
     assert len(np.shape(music)) == 2
-
+    
     music_1 = replace_delay(music, -1)
     music_2 = replace_delay(music_1, 0)
+    nonzero_cnt = np.count_nonzero(music_1, axis=1)
 
-    return repeatness(music_1, threshold_r)*weight_r + fluctuation(music_2, threshold_f, threshold_m)*weight_f
+    score = repeatness(music_1, threshold_r)*weight_r + fluctuation(music_2, nonzero_cnt, threshold_f, threshold_m)*weight_f
+    return 2*score
     
     
